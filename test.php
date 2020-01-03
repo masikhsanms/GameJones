3690d3ca9f8595006108b1df15bf3d53cef4a2bc
Ikhsan MS <31879065+masikhsanms@users.noreply.github.com>
2020-01-03 11:37:05 +0700

    test.php
    comment print html

--- test.php
+++ test.php
@@ -5,6 +5,6 @@ $html = file_get_contents($url);
 // $arr = explode('window._sharedData = ',$html);
 // $arr = explode(';</script>',$arr[1]);
 // $obj = json_decode($arr[0] , true);
-print_r($html);
+//print_r($html);
 
  ?>
